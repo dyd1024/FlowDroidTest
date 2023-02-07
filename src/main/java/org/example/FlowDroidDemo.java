f1792c178725c5d82d9b37a8c66270ea7029fa9c
dyd1024 <dyd1024@163.com>
2023-02-07 09:00:12 +0800

    update

--- src/main/java/org/example/FlowDroidDemo.java
+++ src/main/java/org/example/FlowDroidDemo.java
@@ -1,13 +1,9 @@
 package org.example;
 
-import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.BoomerangSolver.Utility;
-import de.fraunhofer.iem.secucheck.analysis.query.EntryPoint;
-import soot.Scene;
-import soot.SootClass;
+//import de.fraunhofer.iem.secucheck.analysis.implementation.SingleFlowTaintAnalysis.BoomerangSolver.Utility;
 import soot.jimple.infoflow.Infoflow;
 import soot.jimple.infoflow.InfoflowConfiguration;
 import soot.jimple.infoflow.config.IInfoflowConfig;
-import soot.jimple.infoflow.entryPointCreators.DefaultEntryPointCreator;
 import soot.jimple.infoflow.results.DataFlowResult;
 import soot.jimple.infoflow.results.InfoflowResults;
 import soot.jimple.infoflow.taintWrappers.EasyTaintWrapper;
@@ -15,19 +11,18 @@ import soot.options.Options;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Iterator;
 import java.util.List;
 
 public class FlowDroidDemo {
     protected static List<String> sources;
     protected static List<String> sinks;
-    protected static String classname = "test.testFastjsonDev";
-    protected static final String source1 = "<gov.nasa.jpf.jdart.SymbolicString: java.lang.String makeConcolicString(java.lang.String)>";
-    protected static final String sink1 = "<com.alibaba.fastjson.parser.deserializer.IntegerDeserializer: java.lang.Object deserialze(com.alibaba.fastjson.parser.DefaultJSONParser,java.lang.reflect.Type,java.lang.Object)>";
-//    protected static final String sink1 = "<com.alibaba.fastjson.parser.deserializer.ObjectDeserializer: java.lang.Object deserialze(com.alibaba.fastjson.parser.DefaultJSONParser,java.lang.reflect.Type,java.lang.Object)>";
-//    protected static final String sink1 = "<com.alibaba.fastjson.parser.DefaultJSONParser: java.lang.Object parse()>";
-//    protected static final String sink1 = "<com.alibaba.fastjson.parser.DefaultJSONParser: java.lang.Object parseObject(java.util.Map,java.lang.Object)>";
+    protected static String classname = "org.testCase.StringAppend";
+    protected static final String source1 = "<org.testCase.SourceSink: java.lang.String source()>";
+    protected static final String sink1 = "<org.testCase.SourceSink: void sink(java.lang.String)>";
+    protected static final String sink2 = "<org.testCase.SourceSink: void sink(java.lang.String,int)>";
+    protected static final String sink3 = "<org.testCase.SourceSink: void sink(java.lang.String,java.lang.String)>";
 
     protected static final String entry_point = "<"+classname+": void main(java.lang.String[])>";
 
@@ -38,13 +33,15 @@ public class FlowDroidDemo {
 //     libPath = System.getProperty("java.home") + File.separator + "lib" + File.separator + "rt.jar";
         libPath = "";
 
-        appPath = "/home/dyd/project/jpf-concolic/jpf-concolic/build-idea";
+        appPath = "target/classes";
 
         sources = new ArrayList<String>();
         sources.add(source1);
 
         sinks = new ArrayList<String>();
         sinks.add(sink1);
+        sinks.add(sink2);
+        sinks.add(sink3);
 
         List<String> epoints = new ArrayList<String>();
         epoints.add(entry_point);
@@ -76,7 +73,7 @@ public class FlowDroidDemo {
             public void setSootOptions(Options options, InfoflowConfiguration config) {
                 // TODO: set included packages.
                 // options.set_include(includeList);
-                options.set_exclude(Utility.excludedPackages());
+                options.set_exclude(excludedPackages());
                 options.set_output_format(Options.output_format_none);
             }
         });
@@ -85,5 +82,19 @@ public class FlowDroidDemo {
         infoFlow.getConfig().setLogSourcesAndSinks(true);
         return infoFlow;
     }
+
+    public static List<String> excludedPackages() {
+        List<String> excludedPackages = new LinkedList<>();
+        excludedPackages.add("sun.*");
+        excludedPackages.add("javax.*");
+        excludedPackages.add("com.sun.*");
+        excludedPackages.add("com.ibm.*");
+        excludedPackages.add("org.xml.*");
+        excludedPackages.add("org.w3c.*");
+        excludedPackages.add("apple.awt.*");
+        excludedPackages.add("com.apple.*");
+        return excludedPackages;
+    }
+
 
 }
